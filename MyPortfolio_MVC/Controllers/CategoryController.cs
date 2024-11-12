afb08dfe069b819445323e9e758a65d47ecafd73
ankaz <ankaz@I>
2024-11-12 19:09:56 +0300

    Add project files.

---
+++ MyPortfolio_MVC/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using MyPortfolio_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio_MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        MyPortfolioEntities db = new MyPortfolioEntities();
+       
+        
+        public ActionResult Index()
+        {
+           var values =db.TblCategories.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+
+        public ActionResult CreateCategory()
+        {
+            return View();  
+        }
+
+        [HttpPost]
+
+        public ActionResult CreateCategory(TblCategory category)
+        {
+           db.TblCategories.Add(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");   
+        }
+
+    }
+}
